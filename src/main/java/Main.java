f15d5ccedb501fceff367ba0de09f8d6afed15b2
sdavidpinto <sdavidpinto@unicesar.edu.co>
2026-09-02 09:30:53 -0500

    feat: inicializar proyecto Maven de Gamezone

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Samue
+ */
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
