ac43cd3573ed966a4668070debcbc2b8c1b0432b
José Gabriel Quintero Ortiz <jgabrielquintero@unicesar.edu.co>
2026-09-05 17:54:29 -0500

    feat: agregar clase abstracta Person con atributos comunes

---
+++ src/main/java/com/gamezone/model/Person.java
@@ -0,0 +1,37 @@
+package com.gamezone.model;
+
+public abstract class Person {   
+   private String name;
+   private String idNumber;
+   private String phone;
+   
+    public Person(String name, String idNumber, String phone) {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    } 
+}
