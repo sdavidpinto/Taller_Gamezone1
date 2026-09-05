8c127ea49de0fc0931ce1c09d86a7df52ba1448b
Edward <edwardcarrillo@unicesar.edu.co>
2026-09-05 18:30:50 -0500

    fix: actualizar clase VideoGame con sus atributos y metodos

--- src/main/java/model/VideoGame.java
+++ src/main/java/model/VideoGame.java
@@ -12,12 +12,13 @@ public class VideoGame extends Product {
         this.platform = platform;
         this.genre = genre;
         this.ageRating = ageRating;
+        
     }
-
+    
     public String getPlatform() { return platform; }
     public String getGenre() { return genre; }
     public String getAgeRating() { return ageRating; }
-
+    
     @Override
     public String getDescription() {
         return String.format("%s [Video Game] - Platform: %s | Genre: %s | Age rating: %s | Price: $%.2f | Stock: %d",
