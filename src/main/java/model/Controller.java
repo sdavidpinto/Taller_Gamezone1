2899cf03f6a7be1fa38036a2c099afd8777b8eee
Edward <edwardcarrillo@unicesar.edu.co>
2026-09-07 09:54:02 -0500

    feat:implementacion de la subclase Cable que hereda de Accessory

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -6,34 +6,36 @@ package model;
  */
 public class Controller extends Accessory {
 
-    private boolean ConnectionType;
+    private boolean alambric;
 
     /**
-     * Construye una nueva instancia de VideoGame con los detalles especificados.
+     * Construye una nueva instancia de Control con los detalles especificados.
      *
      * @param identifier El identificador único del videojuego.
      * @param title El Nombre del Control.
      * @param price El precio del Control.
      * @param availableQuantity La cantidad disponible en inventario.
-     * @param ConnectionType Si la clase de conexion es alambrica o inalambrica.
+     * @param alambric Si la clase de conexion es alambrica o inalambrica.
      */
     
-    public Controller( String identifier, String title, double price, int availableQuantity, String brand, boolean ConnectionType) {
+    public Controller( String identifier, String title, double price, int availableQuantity, String brand, boolean alambric) {
         super(identifier, title, price, availableQuantity, brand);
-        this.ConnectionType = ConnectionType;
+        this.alambric = alambric;
     }
 
     @Override
     public String getDescription() {
-        return String.format("%s [Controller] %s | Price: $%.2f | Stock: %s | Brand %s ! Connectiontype %d",
-                getTitle(), getPrice(), getAvailableQuantity(), getBrand(), isConnectionType());
+        return String.format("%s [Controller] %s | Price: $%.2f | Stock: %s | Brand %s ! Alambric %d",
+                getTitle(), getPrice(), getAvailableQuantity(), getBrand(), isAlambric());
     }
 
-    public boolean isConnectionType() {
-        return ConnectionType;
+    public boolean isAlambric() {
+        return alambric;
     }
 
-    public void setConnectionType(boolean ConnectionType) {
-        this.ConnectionType = ConnectionType;
+    public void setAlambric(boolean alambric) {
+        this.alambric = alambric;
     }
+    
 }
+
